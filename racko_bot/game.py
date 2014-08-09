24b7eeb00ae934d6248115c8f164e00c94468800
Eric Bower <neurosnap@gmail.com>
2014-08-08 21:32:02 -0400

    removed items

--- racko_bot/game.py
+++ racko_bot/game.py
@@ -1,7 +1,7 @@
 import random
 
 
-class Racko(object):
+class Dealer(object):
 	""" https://en.wikipedia.org/wiki/Rack-O """
 
 	def __init__(self, num_cards, num_players):
@@ -12,15 +12,23 @@ class Racko(object):
 		if num_cards not in [40, 50, 60]:
 			raise ValueError("The number of cards must be 40, 50, or 60")
 		self.num_cards = num_cards
+		self.discard_deck = []
 
 	def shuffle_deck(self):
-		return random.sample(range(1, 60), self.num_cards)
+		return random.sample(range(1, 60), self.num_cards-1)
 
 	def draw_card(self):
 		if self.deck:
-			return self.deck.pop()
+			self.discard_deck.append(self.deck.pop())
+
+	def top_card(self):
+		return self.discard_deck[-1]
 
 	def deal(self):
 		self.deck = self.shuffle_deck()
-		deal = [self.deal() for player in num_players]
+		dealt = []
+		for player in range(0, self.num_players):
+			dealt.append(self.deck[-10:])
+			del self.deck[-10:]
 		self.draw_card()
+		return dealt
